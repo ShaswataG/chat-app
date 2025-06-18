09711a2935edca98be802f893dfb4160b04eafaf
ShaswataG <gshaswata1@gmail.com>
2025-06-18 16:54:31 +0530

    db models

---
+++ backend/src/models/chat.model.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const chatSchema = new mongoose.Schema({
+  room: { type: String, required: true },
+  username: { type: String, required: true },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+export const Chat = mongoose.model('Chat', chatSchema);
