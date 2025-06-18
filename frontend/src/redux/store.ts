f65e464c4487a32cc13b5c0293eb8e01ca29a782
ShaswataG <gshaswata1@gmail.com>
2025-06-18 22:24:17 +0530

    persist user slice in redux

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,13 +1,45 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from './chats/chatReducer';
 import roomReducer from './rooms/roomReducer';
+import userReducer from './users/userReducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 
-export const store = configureStore({
-  reducer: {
-    chat: chatReducer,
-    room: roomReducer,
-  },
+const rootReducer = combineReducers({
+  chat: chatReducer,
+  room: roomReducer,
+  user: userReducer,
 });
 
+const persistedReducer = persistReducer(
+  {
+    key: 'root',
+    storage,
+    whitelist: ['user'],
+  },
+  rootReducer
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
