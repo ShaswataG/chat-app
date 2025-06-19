8e9ef0dada983c1f81f16a93c41b5e41c9a8657f
ShaswataG <gshaswata1@gmail.com>
2025-06-19 07:24:19 +0530

    fix: joined_rooms_ids in User schema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,24 +1,22 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    joined_rooms_ids: {
-        type: [
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        joined_rooms_ids: [
             {
                 type: mongoose.Schema.Types.ObjectId,
                 ref: 'Room'
             }
         ]
-    }
-},
+    },
     {
         timestamps: true
     }
