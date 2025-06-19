904ad12fc1583e27e903d2fc9201bd020bdb58b6
ShaswataG <gshaswata1@gmail.com>
2025-06-19 05:35:21 +0530

    feat: admin_id, timestamp in room schema

--- backend/src/models/room.model.ts
+++ backend/src/models/room.model.ts
@@ -5,7 +5,14 @@ const roomSchema = new mongoose.Schema({
     type: String, 
     required: true, 
     unique: true 
+  },
+  admin_id: {
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
   }
-});
+}, {
+  timestamps: true}
+);
 
 export const Room = mongoose.model('Room', roomSchema);
